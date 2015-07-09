6376d682a9797a74598099901158aed05ba77571
Ricardo Morin <morinricardo@gmail.com>
2015-07-09 16:18:56 -0700

    Started adding globe display for all tracks

--- js/main.js
+++ js/main.js
@@ -6,37 +6,34 @@
 // The map is global
 var map;
 
-// Set up map only after the DOM is ready
 function setUpMap() {
 	// Get trackId from URL query string
 	var trackId = tmConfig.getTrackId();
 
-	// console.log(tmConfig.getUrlVars()["track"]);
-
-	map = new L.map('map');
-
-	// Set up common stuff and return layer control which we will need later on
-	var layerControl = tmMap.setUpCommon();
-
 	// Get track data and wait before populating info onto the map
 	tmData.getTrackInfo(function(data) {
+		// Set up common bootstrap stuff and get the regions
+		var regions = tmMap.setUpCommon(data.tracks);
 
-		// Set up goto menu and grab regions data structure
-		var regions = tmMap.setUpGotoMenu(data.tracks);
-
-		// Set up tracks menu
-		tmMap.setUpTracksMenu(data.tracks);
-
-		// If we do not have a (valid) track id in the query parameter, then go for all tracks
-		if (!(trackId in data.tracks)) {
-			// Since trackId not found then add all tracks markers to map and display all
-			// But first lets see if we need to zoom into a region
-			var regionParm = tmConfig.getRegion();
-			tmMap.setUpAllTracksView(data.tracks, regions[regionParm]);
-		// Otherwise, go for a single track and its gory details
-		} else {
-			tmMap.setUpSingleTrackView(data.tracks[trackId], layerControl, data.tracks);
-		}
+		// Get JSON layer config file and wait before populating map
+		tmConfig.getLayers(function(l) {
+			if (tmConfig.getGlobeFlag()) {
+				tmMap.setUpGlobe(data.tracks, regions);
+			} else {
+				// Set up map and get layer control which we will need later on
+				var layerControl = tmMap.setUpMap(l);
+				// If we do not have a (valid) track id in the query parameter, then go for all tracks
+				if (!(trackId in data.tracks)) {
+					// Since trackId not found then add all tracks markers to map and display all
+					// But first lets see if we need to zoom into a region
+					var regionParm = tmConfig.getRegion();
+					tmMap.setUpAllTracksView(data.tracks, regions[regionParm]);
+				// Otherwise, go for a single track and its gory details
+				} else {
+					tmMap.setUpSingleTrackView(data.tracks[trackId], layerControl, data.tracks);
+				}
+			}
+		});
 	});	
 }
 
