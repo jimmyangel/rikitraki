393dcfba94e61d26664a4626ff85482b5298340c
Ricardo Morin <morinricardo@gmail.com>
2015-07-17 19:36:00 -0700

    Added terrain view

--- js/main.js
+++ js/main.js
@@ -19,25 +19,30 @@ function setUpMap() {
 		// Set up common bootstrap stuff and get the regions
 		var regions = tmMap.setUpCommon(data.tracks);
 
-		// Get JSON layer config file and wait before populating map
-		tmConfig.getLayers(function(l) {
+		// Now figure out what view to set up
+		if (tmConfig.getTerrainFlag() && (trackId in data.tracks)) {
+			tmMap.setUpSingleTrackTerrainView(data.tracks[trackId]);		
+		} else {
 			if (tmConfig.getGlobeFlag() && !isMobile) {
 				tmMap.setUpGlobe(data.tracks, regions);
 			} else {
-				// Set up map and get layer control which we will need later on
-				var layerControl = tmMap.setUpMap(l);
-				// If we do not have a (valid) track id in the query parameter, then go for all tracks
-				if (!(trackId in data.tracks)) {
-					// Since trackId not found then add all tracks markers to map and display all
-					// But first lets see if we need to zoom into a region
-					var regionParm = tmConfig.getRegion();
-					tmMap.setUpAllTracksView(data.tracks, regions[regionParm]);
-				// Otherwise, go for a single track and its gory details
-				} else {
-					tmMap.setUpSingleTrackView(data.tracks[trackId], layerControl, data.tracks);
-				}
+				// Get JSON layer config file and wait before populating map
+				tmConfig.getLayers(function(l) {
+					// Set up map and get layer control which we will need later on
+					var layerControl = tmMap.setUpMap(l);
+					// If we do not have a (valid) track id in the query parameter, then go for all tracks
+					if (!(trackId in data.tracks)) {
+						// Since trackId not found then add all tracks markers to map and display all
+						// But first lets see if we need to zoom into a region
+						var regionParm = tmConfig.getRegion();
+						tmMap.setUpAllTracksView(data.tracks, regions[regionParm]);
+					// Otherwise, go for a single track and its gory details
+					} else {
+						tmMap.setUpSingleTrackView(data.tracks[trackId], layerControl, data.tracks);
+					}
+				});
 			}
-		});
+		}
 	});	
 }
 
