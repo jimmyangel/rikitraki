af06cdc54155e6ac471d773a92f92a4c59f043c5
Ricardo Morin <morinricardo@gmail.com>
2016-02-15 19:01:15 -0800

    Encapsulate messages

---
+++ js/messages.js
@@ -0,0 +1,23 @@
+'use strict';
+/* exported tmMessages */
+var tmMessages = {
+    VALID_USERNAME_PASSWORD: 'Please enter valid username and password',
+    ACCOUNT_MUST_BE_ACTIVATED: 'Account must be activated before use. Please check your email',
+    UNEXPECTED_ERROR: 'Oops! An unexpected error occurred. Please try again later',
+    ENTER_EMAIL: 'Please enter a valid email',
+    NO_EMAIL_FOUND: 'No record found for this email address',
+    USERNAME_EMAIL_EXIST: 'Username or email already exist',
+    VALID_USERNAME: 'Username must be 6 to 40 characters and have no special characters',
+    VALID_PASSWORD: 'Password must be between 6 and 18 characters long',
+    REPASSWORD_MATCH: 'Re-entered password must match password',
+    SELECT_GPX: 'Please select a GPX file for this track (up to 4MB size)',
+    ENTER_TRACK_NAME: 'Please enter a name for this track',
+    ENTER_TRACK_DESCRIPTION: 'Please enter a description for this track',
+    SELECT_REGION: 'Please select a region for this track',
+    VALID_NUMBER: 'Please enter a valid number',
+    SELECT_GOOD_GPX: 'Please select a "good" GPX file (valid format with at least a track segment with elevation data)',
+    SAVE_ERROR: 'Save error, status ',
+    DELETE_PIC_ERROR: 'Delete picture error, status ',
+    UPLOAD_PIC_ERROR: 'Upload picture error, status ',
+    REMOVE_ERROR: 'Remove error, status '
+};
