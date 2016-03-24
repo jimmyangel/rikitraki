baccadd841c4d317a4f48c8d75dbb0d05d5abb84
Ricardo Morin <morinricardo@gmail.com>
2016-03-23 18:30:40 -0700

    Async sample terrain for marker heights

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 'use strict';
-/* exported tmConfig, tmMessages, tmConstants */
+/* exported tmConfig, tmMessages, tmConstants, tmBaseMapLayers */
 
 var tmConfig = (function () {
 	// Development port numbers
@@ -132,41 +132,41 @@ var tmMessages = {
 
 var tmBaseMapLayers = [
 		{
-			layerName: "ESRI World Topo",
-			layerUrl: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}",
-			attribution: "Tiles © Esri — Source: <a href=\"http://www.arcgis.com/home/item.html?id=30e5fe3149c34df1ba922e6f5bbf808f\">ArcGIS World Topographic Map</a>",
+			layerName: 'ESRI World Topo',
+			layerUrl: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Topo_Map/MapServer/tile/{z}/{y}/{x}',
+			attribution: 'Tiles © Esri — Source: <a href="http://www.arcgis.com/home/item.html?id=30e5fe3149c34df1ba922e6f5bbf808f">ArcGIS World Topographic Map</a>',
 			maxZoom: 19
 		},
 		{
-			layerName: "Thunder Forest Outdoors",
-			layerUrl: "https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png",
-			attribution: "© <a href=\"http://www.opencyclemap.org\">OpenCycleMap</a>, &copy; <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a> contributors",
+			layerName: 'Thunder Forest Outdoors',
+			layerUrl: 'https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png',
+			attribution: '© <a href="http://www.opencyclemap.org">OpenCycleMap</a>, © <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 			maxZoom: 17
 		},
 		{
-			layerName: "OpenStreetMap",
-			layerUrl: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-			attribution: "© <a href=\"https://www.openstreetmap.org/copyright\">OpenStreetMap</a>",
+			layerName: 'OpenStreetMap',
+			layerUrl: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+			attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 			maxZoom: 19
 
 		},
 		{
-			layerName: "ESRI World Imagery",
-			layerUrl: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-			attribution: "Tiles © Esri — Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+			layerName: 'ESRI World Imagery',
+			layerUrl: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+			attribution: 'Tiles © Esri — Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community',
 			maxZoom: 19,
 			default3D: true
 		},
 		{
-			layerName: "USGS USA Topo",
-			layerUrl: "https://basemap.nationalmap.gov/ArcGIS/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}",
-			attribution: "<a href=\"https://www.doi.gov\">U.S. Department of the Interior</a> | <a href=\"http://www.usgs.gov\">U.S. Geological Survey</a> | <a href=\"http://www.usgs.gov/laws/policies_notices.html\">Policies</a>",
+			layerName: 'USGS USA Topo',
+			layerUrl: 'https://basemap.nationalmap.gov/ArcGIS/rest/services/USGSTopo/MapServer/tile/{z}/{y}/{x}',
+			attribution: '<a href="https://www.doi.gov">U.S. Department of the Interior</a> | <a href="http://www.usgs.gov">U.S. Geological Survey</a> | <a href="http://www.usgs.gov/laws/policies_notices.html">Policies</a>',
 			maxZoom: 15
 		},
 		{
-			layerName: "ArcGIS USA Topo Maps",
-			layerUrl: "https://services.arcgisonline.com/arcgis/rest/services/USA_Topo_Maps/MapServer/tile/{z}/{y}/{x}",
-			attribution: "Esri, DeLorme, FAO, USGS, NOAA, EPA | Copyright:© 2013 National Geographic Society, i-cubed",
+			layerName: 'ArcGIS USA Topo Maps',
+			layerUrl: 'https://services.arcgisonline.com/arcgis/rest/services/USA_Topo_Maps/MapServer/tile/{z}/{y}/{x}',
+			attribution: 'Esri, DeLorme, FAO, USGS, NOAA, EPA | © 2013 National Geographic Society, i-cubed',
 			maxZoom: 15
 		}
 ];
