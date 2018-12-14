ab80fe44fbe688862ac8184fdad9ccdef07063bf
Ricardo Morin <morinricardo@gmail.com>
2018-12-14 15:34:05 -0800

    Point to services url

--- js/config.js
+++ js/config.js
@@ -8,12 +8,7 @@ export var tmConfig = (function () {
 
 	// This function assumes that the api and the web page are colocated, if that is not the case it will need to be changed
 	var getApiBaseUrl = function () {
-		return 'https://services.rikitraki.com/api'; // Comment this out before pushing to prod
-		/* if (window.location.port === DEV_WEB_PORT) {
-			return window.location.protocol + '//' + window.location.hostname + ':' + DEV_WS_PORT + '/api';
-		}
-		// Hosted in same nodejs container (local or remote)
-		return window.location.protocol + '//' + window.location.host + '/api'; */
+		return 'https://services.rikitraki.com/api';
 	};
 
 	var getTrackId = function () {
