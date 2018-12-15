7047b9d36f61c15a0fbe3569e616548e319e6ee1
Ricardo Morin <morinricardo@gmail.com>
2018-12-15 13:45:16 -0800

    Simplify config

--- js/config.js
+++ js/config.js
@@ -1,93 +1,61 @@
 'use strict';
 
-export var tmConfig = (function () {
+export var tmConfig = {
+  	getApiBaseUrl: function () {
+  		return 'https://services.rikitraki.com/api';
+  	},
+  	getTrackId: function () {
+  		return this.getUrlVars().track;
+  	},
+  	getRegion: function () {
+  		return decodeURIComponent((this.getUrlVars().region));
+  	},
+  	getGlobeFlag: function () {
+  		return this.getUrlVars().globe === 'yes' ? true : false;
+  	},
+  	getOverride: function () {
+  		return this.getUrlVars().override === 'yes' ? true : false;
+  	},
+  	getTerrainFlag: function () {
+  		return this.getUrlVars().terrain === 'yes' ? true : false;
+  	},
 
-	// Development port numbers
-	// var DEV_WEB_PORT = '8080';
-	// var DEV_WS_PORT = '3000';
-
-	// This function assumes that the api and the web page are colocated, if that is not the case it will need to be changed
-	var getApiBaseUrl = function () {
-		return 'https://services.rikitraki.com/api';
-	};
-
-	var getTrackId = function () {
-		return getUrlVars().track;
-	};
-
-	var getRegion = function () {
-		return decodeURIComponent((getUrlVars().region));
-	};
-
-	var getGlobeFlag = function () {
-		return getUrlVars().globe === 'yes' ? true : false;
-	};
-
-	var getOverride = function () {
-		return getUrlVars().override === 'yes' ? true : false;
-	};
-
-	var getTerrainFlag = function () {
-		return getUrlVars().terrain === 'yes' ? true : false;
-	};
-
-	var getVDPlayFlag = function () {
-		return getUrlVars().vdplay === 'yes' ? true : false;
-	};
-
-	var getLayers = function (f) {
-		$.getJSON('config/layers.json', f).fail(function(jqxhr, textStatus, error) {throw error;});
-		return f; //f is the function that is invoked when the data is ready
-	};
-
-	var getUrlVars = function () {
-		var urlVars = [];
-		var varArray = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
-		for (var i = 0; i < varArray.length; i++) {
-			var urlVar = varArray[i].split('=');
-			urlVars[urlVar[0]] = urlVar[1];
-		}
-		return urlVars;
-	};
-
-	var getWebGlFlag = function () {
-		// Basic test
-		if (!window.WebGLRenderingContext) {
-			return false;
-		}
-		// Ok basic test passed, but can WebGL be initialized?
-		var c = document.createElement('canvas');
-		var webglOptions = {
-			alpha : false,
-			stencil : false,
-			failIfMajorPerformanceCaveat : true
-		};
-		var gl = c.getContext('webgl', webglOptions) || c.getContext('experimental-webgl', webglOptions) || undefined;
-		if (!gl) {
-			return false;
-		}
-		// This will catch some really crappy versions like IE on virtualized environment
-		if (gl.getSupportedExtensions().indexOf('OES_standard_derivatives') < 0) {
-			return false;
-		}
-		// If I got here, it WebGL "should" be supported
-		return true;
-	};
-
-	// tmConfig public API
-	return {
-		getApiBaseUrl: getApiBaseUrl,
-		getTrackId: getTrackId,
-		getRegion: getRegion,
-		getGlobeFlag: getGlobeFlag,
-		getOverride: getOverride,
-		getTerrainFlag: getTerrainFlag,
-		getVDPlayFlag: getVDPlayFlag,
-		getLayers: getLayers,
-		getUrlVars: getUrlVars,
-		getWebGlFlag: getWebGlFlag
-	};
-})();
+  	getVDPlayFlag: function () {
+  		return this.getUrlVars().vdplay === 'yes' ? true : false;
+  	},
+  	getUrlVars: function () {
+  		var urlVars = [];
+  		var varArray = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  		for (var i = 0; i < varArray.length; i++) {
+  			var urlVar = varArray[i].split('=');
+  			urlVars[urlVar[0]] = urlVar[1];
+  		}
+  		return urlVars;
+  	},
+  	getWebGlFlag: function () {
+  		// Basic test
+  		if (!window.WebGLRenderingContext) {
+  			return false;
+  		}
+  		// Ok basic test passed, but can WebGL be initialized?
+  		var c = document.createElement('canvas');
+  		var webglOptions = {
+  			alpha : false,
+  			stencil : false,
+  			failIfMajorPerformanceCaveat : true
+  		};
+  		var gl = c.getContext('webgl', webglOptions) || c.getContext('experimental-webgl', webglOptions) || undefined;
+  		if (!gl) {
+  			return false;
+  		}
+  		// This will catch some really crappy versions like IE on virtualized environment
+  		if (gl.getSupportedExtensions().indexOf('OES_standard_derivatives') < 0) {
+  			return false;
+  		}
+  		// If I got here, it WebGL "should" be supported
+  		return true;
+  	}
+}
 
 export var tmConstants = {
 	TRAIL_MARKER_COLOR: '7A5C1E',
