5c41841169f0756cbcb04e8716d1037c48f11d2c
Ricardo Morin <morinricardo@gmail.com>
2019-10-05 17:16:11 -0700

    Upgrade Cesium to 1.62, add mising marker.png file

--- js/config.js
+++ js/config.js
@@ -2,8 +2,8 @@
 
 export var tmConfig = {
   	getApiBaseUrl: function () {
-  		return 'http://localhost:3000/api';
-      // return 'https://services.rikitraki.com/api';
+  		// return 'http://localhost:3000/api';
+      return 'https://services.rikitraki.com/api';
   	},
   	getTrackId: function () {
   		return this.getUrlVars().track;
